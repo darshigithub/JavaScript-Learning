f203a112f095350fd2afe511da34ae09f275252f
Darshan A R <143487265+darshigithub@users.noreply.github.com>
2025-08-06 14:58:16 +0000

    Operators

---
+++ Operators/1_Op.js
@@ -0,0 +1,51 @@
+// Operators in js
+
+let value = 3;
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(3 + 3); // 6
+// console.log(4 - 3); // 1
+// console.log(4 * 5); // 20
+// console.log(7 / 3); // 2.333335
+// console.log(9 % 4); // 1
+// console.log(2 ** 3); // 8 
+
+
+// console.log(1 + "2");
+// console.log("2" + 1);
+// console.log(1 + 2 + "3");
+// // adding the numbers to string will return string
+// console.log("1" + 2 + 3); 
+
+// unary operator 
+
+// increment, decrement
+
+// ++, --
+
+let a = 10;
+
+// post-increment
+console.log(a++); // 10
+console.log(a); // 11
+
+let b = 12;
+
+// post-decrement
+console.log(a--); // 10
+console.log(a); // 9
+
+let c = 34;
+
+// pre-increment
+console.log(++c); // 35
+console.log(c); // 35
+
+let d = 45;
+
+// pre-decrement
+console.log(--d); // 44
+console.log(d); // 44
---
+++ Operators/2_Op.js
@@ -0,0 +1,28 @@
+// Comparision or Reletional operators
+
+// < > >= <= == != 
+
+// console.log(4 > 5); // false
+// console.log(5 < 6); // true
+// console.log(6 >= 5); // true
+// console.log(7 <= 1); // false
+// console.log(8 == 3); // false
+// console.log(7 != 4); // true
+
+
+// In js string -> number while comparing with number
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+// console.log("2" < 1); // false
+
+
+// In js null -> 0 
+console.log(null > 0); // 0 > 0 // false
+console.log(null < 0); // 0 < 0 // false
+console.log(null >= 0); // 0 >= 0 // true
+
+// Strict version of the == -> === and != -> !==
+
+console.log("2" === 2); // false
+
+console.log("3" !== 2); // true 
