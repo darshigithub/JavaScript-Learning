11221e06786d5ce11bf21529cc06af3a105f659a
Darshan A R <143487265+darshigithub@users.noreply.github.com>
2025-08-06 15:35:49 +0000

    datatypes

---
+++ Basics/SummaryOfDatatypes.js
@@ -0,0 +1,34 @@
+// Datatypes
+
+// 2 types of datatypes
+
+// Primitive 
+
+// number, boolean, string, null, undefined, BigInt, Symbol
+
+let value = 10;
+let anotherValue = 12.45;
+let isFallow = true;
+let userName = "Darshan";
+let temp = null;
+let userCupon; // undefined
+
+
+// Reference type (Non - primitive)
+
+// Array, Object, Functions
+
+let nums = [1,2,3,4,5];
+
+let myObj = {
+    name: "Darshan",
+    age: 22
+}
+
+function hello() {
+    console.log("Hello World");
+}
+
+let myFunction = function() {
+    console.log("Hello");
+}
