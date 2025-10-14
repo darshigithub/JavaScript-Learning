56b6a87123be5b80bf6a5a89cf999889236cf1fa
Darshan A R <143487265+darshigithub@users.noreply.github.com>
2025-10-14 07:28:30 +0000

    loops

---
+++ 12_Loops/loop1.js
@@ -0,0 +1,37 @@
+// Loops in JS
+
+// for loop
+
+// print 1 to 10 numbers
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(`${i}`);
+// }
+
+// print table of 1 to 3
+
+// for(let i = 1; i <= 3; i++) {
+//     console.log(`-----${i} th table-----`);
+//     for(let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i * j}`);
+//     }
+// }
+
+
+// break
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+//     console.log(`${i}`);
+// }
+
+// continue 
+
+// for (let i = 1; i <= 10; i++) {
+//     if(i === 5) {
+//         continue;
+//     }
+//     console.log(`${i}`);
+// }
---
+++ 12_Loops/loop2.js
@@ -0,0 +1,27 @@
+// while and do-while loop
+
+// print 1 to n numbers using while
+
+// let x = 1;
+// while(x <= 10) {
+//     console.log(`${x}`);
+//     x++;
+// }
+
+// print 1 to n numbers using do-while
+
+// let y = 1;
+// do{
+//     console.log(`${y}`);
+//     y++;
+// } while(y <= 10);
+
+// print array ele using while
+
+let myArr = ["darshan", "sudeep", "yesh", "Rishab"];
+
+let i = 0;
+while(i < myArr.length) {
+    console.log(`${myArr[i]}`);
+    i++;
+}
