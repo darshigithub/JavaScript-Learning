dc856b17e4fc8d9274549a712eb32d1ae40e0ec1
Darshan A R <143487265+darshigithub@users.noreply.github.com>
2025-10-14 06:31:46 +0000

    conditionals

---
+++ 11_Conditionals/c1.js
@@ -0,0 +1,43 @@
+// contionals 
+
+// if 
+
+// let userLogin = true;
+
+// if(userLogin) {
+//     console.log("Welcome to Home Page");
+// }
+
+
+// if(2 == "2") { // but integer != String
+//     console.log("Equal");
+// }
+
+// if(2 === "2") {
+//     console.log("Equal");    
+// }
+
+
+// if-else 
+
+// let age = 18; // age > 18 -> eligible for vote
+
+// if(age >= 18) {
+//     console.log("Eligible For Vote");
+// } else {
+//     console.log("Not Eligible For Vote");
+// }
+
+// else -if
+
+// let score = 50;
+
+// if (score > 90) {
+//     console.log("Top Class player");
+// } else if (score > 70 && score <= 90) {
+//     console.log("Good Player");
+// } else if (score > 30 && score <= 70) {
+//     console.log("Medium player");
+// } else {
+//     console.log("Impact less player");
+// }
---
+++ 11_Conditionals/c2.js
@@ -0,0 +1,57 @@
+// switch case
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let month = 2
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     case 4:
+//         console.log("Apr");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+//     default:
+//         console.log("Invalid Choice");
+// }
+
+
+// +++++++++++++  Truthy and Falsy  +++++++++++++
+
+// let userEmail = "darshan@gmail.com"; 
+// // String -> empty -> falsy value
+// // String -> not empty -> truthy value
+
+// if (userEmail) {
+//     console.log("Got the user");
+// } else {
+//     console.log("Don't got email");
+// }
+
+// falsy value -> false, 0, -0, NaN, null, undefined, "", BigInt 0n
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+// nullish coalescing operator (??) in JavaScript
+
+
+let val;
+// val = 10 ?? 5;
+
+// val = null ?? 6; 
+// val = undefined ?? null;
+console.log(val);
