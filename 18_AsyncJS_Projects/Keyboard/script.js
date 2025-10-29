058205024fd9a68247873be3008af32af9cad660
Darshan A R <143487265+darshigithub@users.noreply.github.com>
2025-10-29 10:12:16 +0000

    keyboard

---
+++ 18_AsyncJS_Projects/Keyboard/script.js
@@ -0,0 +1,20 @@
+let insert = document.querySelector(".insert");
+
+window.addEventListener("keydown",(e) => {
+    insert.innerHTML = `
+        <div class = "text">
+            <table border="1">
+                <tr>
+                    <th>Key</th>
+                    <th>KeyCode</th>
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? "Space" : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+            </table>
+        </div>
+    `;
+});
